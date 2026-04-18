facd9d023817ae5aaa4376f0b6168304c11ad813
Takurou Ogawa <takurouogawa@TakurounoMacBook-Air.local>
2026-04-18 14:33:09 +0900

    add player character

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -15,34 +15,53 @@ public class BoardManager : MonoBehaviour
    public int Height;
    public Tile[] GroundTiles;
    public Tile[] WallTiles;
+   private Grid m_Grid;
+   public Vector3 CellToWorld(Vector2Int cellIndex)
+    {
+        return m_Grid.GetCellCenterWorld((Vector3Int)cellIndex);
+    }
+   public PlayerController Player;
+   public CellData GetCellData(Vector2Int cellIndex)
+    {
+    if (cellIndex.x < 0 || cellIndex.x >= Width
+        || cellIndex.y < 0 || cellIndex.y >= Height)
+    {
+        return null;
+    }
+
+    return m_BoardData[cellIndex.x, cellIndex.y];
+    }
 
    // Start is called before the first frame update
    void Start()
     {
-        m_Tilemap = GetComponentInChildren<Tilemap>();
+    m_Tilemap = GetComponentInChildren<Tilemap>();
+    m_Grid = GetComponentInChildren<Grid>();
+    
+    m_BoardData = new CellData[Width, Height];
 
-        m_BoardData = new CellData[Width, Height];
-
-        for (int y = 0; y < Height; ++y)
+    for (int y = 0; y < Height; ++y)
+    {
+        for(int x = 0; x < Width; ++x)
         {
-            for(int x = 0; x < Width; ++x)
+            Tile tile;
+            m_BoardData[x, y] = new CellData();
+
+            if(x == 0 || y == 0 || x == Width - 1 || y == Height - 1)
             {
-                Tile tile;
-                m_BoardData[x, y] = new CellData();
-
-                if (x == 0 || y == 0 || x == Width - 1 || y == Height - 1)
-                {
-                    tile = WallTiles[Random.Range(0, WallTiles.Length)];
-                    m_BoardData[x, y].Passable = false;
-                }
-                else
-                {
-                    tile = GroundTiles[Random.Range(0, GroundTiles.Length)];
-                    m_BoardData[x, y].Passable = true;
-                }
-
-                m_Tilemap.SetTile(new Vector3Int(x, y, 0), tile);
+                tile = WallTiles[Random.Range(0, WallTiles.Length)];
+                m_BoardData[x, y].Passable = false;
             }
+            else
+            {
+                tile = GroundTiles[Random.Range(0, GroundTiles.Length)];
+                m_BoardData[x, y].Passable = true;
+            }
+
+            m_Tilemap.SetTile(new Vector3Int(x, y, 0), tile);
         }
+    }
+
+    Player.Spawn(this, new Vector2Int(1, 1));
     }
 }
