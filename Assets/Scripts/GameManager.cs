8768fcb2c95c85c8daff26bba801024a9a5b3b8c
Takurou Ogawa <takurouogawa@TakurounoMacBook-Air.local>
2026-04-19 14:16:56 +0900

    add turn count

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+   public static GameManager Instance { get; private set; }
+  
    public BoardManager BoardManager;
    public PlayerController PlayerController;
 
-   private TurnManager m_TurnManager;
+   public TurnManager TurnManager { get; private set;}
+
+   private void Awake()
+   {
+       if (Instance != null)
+       {
+           Destroy(gameObject);
+           return;
+       }
+      
+       Instance = this;
+   }
   
-   // Start is called once before the first execution of Update after the MonoBehaviour is created
    void Start()
    {
-       m_TurnManager = new TurnManager();
+       TurnManager = new TurnManager();
       
        BoardManager.Init();
        PlayerController.Spawn(BoardManager, new Vector2Int(1,1));
--- Assets/Scripts/TurnManager.cs
+++ Assets/Scripts/TurnManager.cs
@@ -2,5 +2,16 @@ using UnityEngine;
 
 public class TurnManager
 {
-  
+   private int m_TurnCount;
+
+   public TurnManager()
+   {
+       m_TurnCount = 1;
+   }
+
+   public void Tick()
+   {
+       m_TurnCount += 1;
+       Debug.Log("Current turn count : " + m_TurnCount);
+   }
 }
