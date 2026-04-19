1e1a903ed8176ecf6226d97e7756696635afd575
Takurou Ogawa <takurouogawa@TakurounoMacBook-Air.local>
2026-04-19 13:37:11 +0900

    fix init

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+   public BoardManager BoardManager;
+   public PlayerController PlayerController;
+
+   private TurnManager m_TurnManager;
+  
+   // Start is called once before the first execution of Update after the MonoBehaviour is created
+   void Start()
+   {
+       m_TurnManager = new TurnManager();
+      
+       BoardManager.Init();
+       PlayerController.Spawn(BoardManager, new Vector2Int(1,1));
+   }
+}
